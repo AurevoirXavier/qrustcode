38fc3e119a0352358ad57ec23e8d98d2874a439c
Xavier Lau <c.estlavie@icloud.com>
2018-09-27 21:46:44 +0800

    21:46/09/27/18

--- src/encoder.rs
+++ src/encoder.rs
@@ -12,9 +12,9 @@ pub struct Encoder {
     //      252 ~ 255 not implemented yet
     modes: [String; 4],
 
-    // indicators_size's index -> version
-    // indicators              -> [indicators' size in different mode]
-    // indicators[mode]        -> indicator's size
+    // indicators_bit's index -> version
+    // indicators              -> [indicators' bits in different mode]
+    // indicators[mode]        -> indicator's bits
     //
     // version:
     //      micro_mode:
@@ -25,7 +25,7 @@ pub struct Encoder {
     //          27 ~ 40 -> 2
     //
     // mode: same as above
-    indicators_size: [[u8; 4]; 3],
+    indicators_bits: [[usize; 4]; 3],
 }
 
 impl Encoder {
@@ -33,7 +33,7 @@ impl Encoder {
         let mut encoder = Encoder {
             micro_mode: Default::default(),
             modes: Default::default(),
-            indicators_size: Default::default(),
+            indicators_bits: Default::default(),
         };
         encoder.set_micro_mode(false);
 
@@ -52,7 +52,7 @@ impl Encoder {
                 String::from("0100"),
                 String::from("1000")
             ];
-            self.indicators_size = [
+            self.indicators_bits = [
                 [10, 9, 8, 8],
                 [12, 1, 16, 10],
                 [14, 13, 16, 12]
@@ -60,8 +60,45 @@ impl Encoder {
         }
     }
 
+    fn binary(mut bits: usize, mut num: usize) -> Vec<bool> {
+        let mut binary = vec![];
+        binary.resize(bits, false);
+
+        while num != 0 {
+            bits -= 1;
+
+            binary[bits] = match num & 1 {
+                0 => false,
+                1 => true,
+                _ => unreachable!()
+            };
+
+            num >>= 1;
+        }
+
+        binary
+    }
+
+    fn numeric_encode(&self, bits: usize, text: &str) -> Vec<bool> {
+        let len = text.len();
+        let mut encode = vec![];
+
+        for i in (0..).step_by(3) {
+            match len - i {
+                bits @ 1...2 => {
+                    encode.push(Encoder::binary(bits * 3 + 1, text[i..len].parse().unwrap()));
+
+                    return encode.concat();
+                }
+                _ => encode.push(Encoder::binary(bits, text[i..i + 3].parse().unwrap()))
+            }
+        }
+
+        unreachable!()
+    }
+
     pub fn encode(&self, mode: &str, version: &str, correction_level: &str, text: &str) {
-        let indicator_size = {
+        let indicator_bits = {
             let mode = match mode {
                 "Numeric" => 0,
                 "Alphanumeric" => 1,
@@ -69,11 +106,11 @@ impl Encoder {
                 "Kanji" => 3,
                 _ => unreachable!() // TODO
             };
-            let indicators_size = self.indicators_size[{
+            let indicators_bits = self.indicators_bits[{
                 if self.micro_mode {
                     unreachable!() // TODO
                 } else {
-                    match version.parse::<u8>().unwrap() {
+                    match version.parse::<usize>().unwrap() {
                         1...9 => 0,
                         10...26 => 1,
                         27...40 => 2,
@@ -82,7 +119,12 @@ impl Encoder {
                 }
             }];
 
-            indicators_size[mode]
+            indicators_bits[mode]
         };
+
+        match mode {
+            "Numeric" => { self.numeric_encode(indicator_bits, text); }
+            _ => unreachable!() // TODO
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
+#![feature(test)]
+extern crate test;
+
 mod encoder;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use self::test::Bencher;
 
-    # [test]
-    fn encoder_test() {
+    #[bench]
+    fn encoder_test(b: &mut Bencher) {
         use self::encoder::Encoder;
-        let mut encoder = Encoder::new();
+        let encoder = Encoder::new();
+
+        b.iter(|| {
+            (0..10000).fold((), |_, _|
+                encoder.encode("Numeric", "1", "H", "01234567")
+            )
+        });
     }
 }
