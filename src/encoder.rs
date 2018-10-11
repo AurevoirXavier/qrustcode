2d1152e7c2b302bf9aab597fb9410c9283322489
Xavier Lau <c.estlavie@icloud.com>
2018-10-12 01:49:32 +0800

    01:49/10/12/18

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,43 +1,100 @@
-use std::collections::HashMap;
+// Number of bits in character count indicator for QR Code
+// indicators_bit's index -> version
+// indicators             -> [indicators' bits in different mode]
+// indicators[mode]       -> indicator's bits
+//
+// version:
+//      micro_mode:
+//          M1 ~ M4 -> 0 ~ 3
+//      normal:
+//          1  ~ 9  -> 0
+//          10 ~ 26 -> 1
+//          27 ~ 40 -> 2
+//
+// mode: same as above
+const INDICATORS: [[u8; 4]; 3] = [
+    [10, 9, 8, 8],
+    [12, 11, 16, 10],
+    [14, 13, 16, 12]
+];
+
+// Error correction characteristics for QR Code
+// total_and_correction's index                   -> version
+// total_and_correction[version]                  -> (total number of codewords, [ec_levels])
+// number of error correction codewords[ec_level] -> number of error correction codewords
+//
+// version:
+//      N/A    -> 0
+//      1 ~ 40 -> 1 ~ 40
+//
+// ec_levels:
+//      L -> 0
+//      M -> 1
+//      Q -> 2
+//      H -> 3
+//
+// codewords[version][ec_level] -> (total number of data codewords for this Version and ec level, ec codewords per block)
+const CODEWORDS: [[(u32, u8); 4]; 40] = [
+    [(19, 7), (16, 10), (13, 13), (9, 17)], [(34, 10), (28, 16), (22, 22), (16, 28)],
+    [(55, 15), (44, 26), (34, 18), (26, 22)], [(80, 20), (64, 18), (48, 26), (36, 16)],
+    [(108, 26), (86, 24), (62, 18), (46, 22)], [(136, 18), (108, 16), (76, 24), (60, 28)],
+    [(156, 20), (124, 18), (88, 18), (66, 26)], [(194, 24), (154, 22), (110, 22), (86, 26)],
+    [(232, 30), (182, 22), (132, 20), (100, 24)], [(274, 18), (216, 26), (154, 24), (122, 28)],
+    [(324, 20), (254, 30), (180, 28), (140, 24)], [(370, 24), (290, 22), (206, 26), (158, 28)],
+    [(428, 26), (334, 22), (244, 24), (180, 22)], [(461, 30), (365, 24), (261, 20), (197, 24)],
+    [(523, 22), (415, 24), (295, 30), (223, 24)], [(589, 24), (453, 28), (325, 24), (253, 30)],
+    [(647, 28), (507, 28), (367, 28), (283, 28)], [(721, 30), (563, 26), (397, 28), (313, 28)],
+    [(795, 28), (627, 26), (445, 26), (341, 26)], [(861, 28), (669, 26), (485, 30), (385, 28)],
+    [(932, 28), (714, 26), (512, 28), (406, 30)], [(1006, 28), (782, 28), (568, 30), (442, 24)],
+    [(1094, 30), (860, 28), (614, 30), (464, 30)], [(1174, 30), (914, 28), (664, 30), (514, 30)],
+    [(1276, 26), (1000, 28), (718, 30), (538, 30)], [(1370, 28), (1062, 28), (754, 28), (596, 30)],
+    [(1468, 30), (1128, 28), (808, 30), (628, 30)], [(1531, 30), (1193, 28), (871, 30), (661, 30)],
+    [(1631, 30), (1267, 28), (911, 30), (701, 30)], [(1735, 30), (1373, 28), (985, 30), (745, 30)],
+    [(1843, 30), (1455, 28), (1033, 30), (793, 30)], [(1955, 30), (1541, 28), (1115, 30), (845, 30)],
+    [(2071, 30), (1631, 28), (1171, 30), (901, 30)], [(2191, 30), (1725, 28), (1231, 30), (961, 30)],
+    [(2306, 30), (1812, 28), (1286, 30), (986, 30)], [(2434, 30), (1914, 28), (1354, 30), (1054, 30)],
+    [(2566, 30), (1992, 28), (1426, 30), (1096, 30)], [(2702, 30), (2102, 28), (1502, 30), (1142, 30)],
+    [(2812, 30), (2216, 28), (1582, 30), (1222, 30)], [(2956, 30), (2334, 28), (1666, 30), (1276, 30)]
+];
+
+fn binary(mut bits_count: usize, mut num: u16) -> Vec<u8> {
+    let mut binary = vec![];
+    binary.resize(bits_count, 0);
+
+    while num != 0 {
+        bits_count -= 1;
+        binary[bits_count] = (num & 1) as u8;
+        num >>= 1;
+    }
+
+    binary
+}
+
+fn decimal(binary: &[u8]) -> u8 {
+    let mut decimal = 0;
+    for (exp, bit) in binary.iter().rev().enumerate() { decimal += bit << exp; }
+
+    decimal
+}
+
+fn alphanumeric_table(b: u8) -> u16 {
+    match b {
+        48...57 => b as u16 - 48, // '0' ~ '9'
+        65...90 => b as u16 - 65, // 'A' ~ 'Z'
+        32 => 36,                 // ' '
+        36 => 37,                 // '$'
+        37 => 38,                 // '%'
+        42 => 39,                 // '*'
+        43 => 40,                 // '+'
+        45 => 41,                 // '-'
+        46 => 42,                 // '.'
+        47 => 43,                 // '/'
+        58 => 44,                 // ':'
+        _ => panic!()
+    }
+}
 
 pub struct Encoder {
-    // Number of bits in character count indicator for QR Code
-    // indicators_bit's index -> version
-    // indicators             -> [indicators' bits in different mode]
-    // indicators[mode]       -> indicator's bits
-    //
-    // version:
-    //      micro_mode:
-    //          M1 ~ M4 -> 0 ~ 3
-    //      normal:
-    //          1  ~ 9  -> 0
-    //          10 ~ 26 -> 1
-    //          27 ~ 40 -> 2
-    //
-    // mode: same as above
-    indicators: [[u8; 4]; 3],
-
-    // Encoding table for Alphanumeric mode
-    alphanumeric_table: HashMap<char, u8>,
-
-    // Error correction characteristics for QR Code
-    // total_and_correction's index                   -> version
-    // total_and_correction[version]                  -> (total number of codewords, [ec_levels])
-    // number of error correction codewords[ec_level] -> number of error correction codewords
-    //
-    // version:
-    //      N/A    -> 0
-    //      1 ~ 40 -> 1 ~ 40
-    //
-    // ec_levels:
-    //      L -> 0
-    //      M -> 1
-    //      Q -> 2
-    //      H -> 3
-    //
-    // codewords[version][ec_level] -> (total number of data codewords for this Version and ec level, ec codewords per block)
-    codewords: [[(u32, u8); 4]; 40],
-
     // log and anti log tables
     gf_exp: [u8; 512],
     gf_log: [u8; 256],
@@ -67,81 +124,21 @@ impl Encoder {
         let (gf_exp, gf_log) = Encoder::init_gf_tables();
 
         Encoder {
-            indicators: [
-                [10, 9, 8, 8],
-                [12, 11, 16, 10],
-                [14, 13, 16, 12]
-            ],
-            alphanumeric_table: [
-                ('0', 0), ('1', 1), ('2', 2), ('3', 3), ('4', 4), ('5', 5),
-                ('6', 6), ('7', 7), ('8', 8), ('9', 9),
-                ('A', 10), ('B', 11), ('C', 12), ('D', 13), ('E', 14), ('F', 15),
-                ('G', 16), ('H', 17), ('I', 18), ('J', 19), ('K', 20), ('L', 21),
-                ('M', 22), ('N', 23), ('O', 24), ('P', 25), ('Q', 26), ('R', 27),
-                ('S', 28), ('T', 29), ('U', 30), ('V', 31), ('W', 32), ('X', 33),
-                ('Y', 34), ('Z', 35),
-                (' ', 36), ('$', 37), ('%', 38), ('*', 39), ('+', 40), ('-', 41),
-                ('.', 42), ('/', 43), (':', 44)
-            ].iter()
-                .map(|&t| t)
-                .collect(),
-            codewords: [
-                [(19, 7), (16, 10), (13, 13), (9, 17)], [(34, 10), (28, 16), (22, 22), (16, 28)],
-                [(55, 15), (44, 26), (34, 18), (26, 22)], [(80, 20), (64, 18), (48, 26), (36, 16)],
-                [(108, 26), (86, 24), (62, 18), (46, 22)], [(136, 18), (108, 16), (76, 24), (60, 28)],
-                [(156, 20), (124, 18), (88, 18), (66, 26)], [(194, 24), (154, 22), (110, 22), (86, 26)],
-                [(232, 30), (182, 22), (132, 20), (100, 24)], [(274, 18), (216, 26), (154, 24), (122, 28)],
-                [(324, 20), (254, 30), (180, 28), (140, 24)], [(370, 24), (290, 22), (206, 26), (158, 28)],
-                [(428, 26), (334, 22), (244, 24), (180, 22)], [(461, 30), (365, 24), (261, 20), (197, 24)],
-                [(523, 22), (415, 24), (295, 30), (223, 24)], [(589, 24), (453, 28), (325, 24), (253, 30)],
-                [(647, 28), (507, 28), (367, 28), (283, 28)], [(721, 30), (563, 26), (397, 28), (313, 28)],
-                [(795, 28), (627, 26), (445, 26), (341, 26)], [(861, 28), (669, 26), (485, 30), (385, 28)],
-                [(932, 28), (714, 26), (512, 28), (406, 30)], [(1006, 28), (782, 28), (568, 30), (442, 24)],
-                [(1094, 30), (860, 28), (614, 30), (464, 30)], [(1174, 30), (914, 28), (664, 30), (514, 30)],
-                [(1276, 26), (1000, 28), (718, 30), (538, 30)], [(1370, 28), (1062, 28), (754, 28), (596, 30)],
-                [(1468, 30), (1128, 28), (808, 30), (628, 30)], [(1531, 30), (1193, 28), (871, 30), (661, 30)],
-                [(1631, 30), (1267, 28), (911, 30), (701, 30)], [(1735, 30), (1373, 28), (985, 30), (745, 30)],
-                [(1843, 30), (1455, 28), (1033, 30), (793, 30)], [(1955, 30), (1541, 28), (1115, 30), (845, 30)],
-                [(2071, 30), (1631, 28), (1171, 30), (901, 30)], [(2191, 30), (1725, 28), (1231, 30), (961, 30)],
-                [(2306, 30), (1812, 28), (1286, 30), (986, 30)], [(2434, 30), (1914, 28), (1354, 30), (1054, 30)],
-                [(2566, 30), (1992, 28), (1426, 30), (1096, 30)], [(2702, 30), (2102, 28), (1502, 30), (1142, 30)],
-                [(2812, 30), (2216, 28), (1582, 30), (1222, 30)], [(2956, 30), (2334, 28), (1666, 30), (1276, 30)]
-            ],
             gf_exp,
             gf_log,
         }
     }
 
-    fn binary(mut bits_count: usize, mut num: u32) -> Vec<u8> {
-        let mut binary = vec![];
-        binary.resize(bits_count, 0);
-
-        while num != 0 {
-            bits_count -= 1;
-            binary[bits_count] = (num & 1) as u8;
-            num >>= 1;
-        }
-
-        binary
-    }
-
-    fn decimal(binary: &[u8]) -> u8 {
-        let mut decimal = 0;
-        for (exp, bit) in binary.iter().rev().enumerate() { decimal += bit << exp; }
-
-        decimal
-    }
-
     fn error_correct(&self, mut encode: Vec<u8>, version: usize, ec_level: usize) -> Vec<u8> {
         for _ in 0..12 - (4 + encode.len()) % 8 { encode.push(0); } // terminator
 
-        let re_codewords = self.codewords[version][ec_level].0 - encode.len() as u32 / 8;
+        let re_cws = CODEWORDS[version][ec_level].0 - encode.len() as u32 / 8;
 
         let mut decimals = vec![];
-        for binary in encode.chunks(8) { decimals.push(Encoder::decimal(binary)); }
+        for binary in encode.chunks(8) { decimals.push(decimal(binary)); }
 
         let mut paddings = [236u8, 17].iter().cycle();
-        for _ in 0..re_codewords { decimals.push(*paddings.next().unwrap()); }
+        for _ in 0..re_cws { decimals.push(*paddings.next().unwrap()); }
 
         decimals
     }
@@ -151,14 +148,14 @@ impl Encoder {
         let edge = len / 3 * 3;
         let mut encode = vec![vec![0, 0, 0, 1]];
 
-        encode.push(Encoder::binary(bits_count as usize, len as u32));
+        encode.push(binary(bits_count as usize, len as u16));
 
         for i in (0..edge).step_by(3) {
-            encode.push(Encoder::binary(bits_count as usize, text[i..i + 3].parse().unwrap()));
+            encode.push(binary(bits_count as usize, text[i..i + 3].parse().unwrap()));
         }
 
         match len - edge {
-            bits @ 1...2 => encode.push(Encoder::binary(1 + 3 * bits, text[edge..len].parse().unwrap())),
+            bits @ 1...2 => encode.push(binary(1 + 3 * bits, text[edge..len].parse().unwrap())),
             _ => ()
         }
 
@@ -167,19 +164,19 @@ impl Encoder {
 
     fn alphanumeric_encode(&self, bits_count: u8, text: &str) -> Vec<u8> {
         let len = text.len();
-        let text: Vec<_> = text.chars().collect();
+        let text = text.as_bytes();
         let mut encode = vec![vec![0, 0, 1, 0]];
 
-        encode.push(Encoder::binary(bits_count as usize, len as u32));
+        encode.push(binary(bits_count as usize, len as u16));
 
         for i in (0..len >> 1 << 1).step_by(2) {
-            encode.push(Encoder::binary(
+            encode.push(binary(
                 11,
-                45 * (*self.alphanumeric_table.get(&text[i]).unwrap()) as u32 + *self.alphanumeric_table.get(&text[i + 1]).unwrap() as u32,
+                45 * alphanumeric_table(text[i]) + alphanumeric_table(text[i + 1]),
             ));
         }
 
-        if len & 1 == 1 { encode.push(Encoder::binary(6, *self.alphanumeric_table.get(&text[len - 1]).unwrap() as u32)); }
+        if len & 1 == 1 { encode.push(binary(6, *text.last().unwrap() as u16)); } else {  }
 
         encode.concat()
     }
@@ -205,7 +202,7 @@ impl Encoder {
             "H" => 3,
             _ => panic!()
         };
-        let bits_counts = self.indicators[match version {
+        let bits_counts = INDICATORS[match version {
             0...8 => 0,
             9...25 => 1,
             26...39 => 2,
@@ -224,8 +221,7 @@ impl Encoder {
             version,
             ec_level,
         );
-        let ec_codewords_per_block = self.codewords[version][ec_level].1;
-
+        let ec_cw_per_block = CODEWORDS[version][ec_level].1;
 
         println!("{:?}", encode);
     }
--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,18 @@ mod tests {
         let encoder = Encoder::new();
 
         // Numeric
-//        b.iter(|| {
-//            (0..1).fold((), |_, _|
-//                encoder.encode("Numeric", "1", "H", "01234567")
-//            )
-//        });
+        b.iter(|| {
+            (0..1).fold((), |_, _|
+                encoder.encode("Numeric", "1", "H", "01234567")
+            )
+        });
 
         // Alphanumeric
         b.iter(|| (0..1).fold((), |_, _| encoder.encode("Alphanumeric", "1", "M", "HELLO WORLD")));
+    }
+
+    #[test]
+    fn p() {
+        println!("{}", b':');
     }
 }
