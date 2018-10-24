84a27221da07c6ada254bb2818662f090cce6b54
Xavier Lau <c.estlavie@icloud.com>
2018-10-24 19:41:29 +0800

    add_finder_patterns - progress 100%

--- src/encoder/matrix.rs
+++ src/encoder/matrix.rs
@@ -1,11 +1,91 @@
-pub struct Matrix {
-    bits: Vec<u8>,
+fn add_finder_patterns(matrix: &mut Vec<Vec<bool>>) {
+    let top_left_corner = matrix.len() - 7;
+
+    for (i, j) in [(0, 0), (top_left_corner, 0), (0, top_left_corner)].iter() {
+        for y in 0..7 {
+            for x in 0..7 {
+                match x {
+                    1 | 5 => match y {
+                        1...5 => continue,
+                        _ => ()
+                    }
+                    _ => ()
+                }
+
+                match y {
+                    1 | 5 => {
+                        match x {
+                            2...4 => continue,
+                            _ => ()
+                        }
+                    }
+                    _ => ()
+                }
+
+                matrix[x + i][y + j] = true;
+            }
+        }
+    }
+}
+
+#[test]
+fn test() {
+    let mut matrix = {
+        let mut matrix = vec![];
+        let mut row = vec![];
+
+        row.resize(21, false);
+        matrix.resize(21, row);
+
+        matrix
+    };
+
+    add_finder_patterns(&mut matrix);
+
+    assert_eq!(
+        matrix.into_iter()
+            .map(|row| row.into_iter()
+                .map(|flag| if flag { '■' } else { ' ' })
+                .collect::<String>()
+            ).collect::<Vec<String>>()
+            .join("\n"),
+        [
+            "\
+            ■■■■■■■       ■■■■■■■\n\
+            ■     ■       ■     ■\n\
+            ■ ■■■ ■       ■ ■■■ ■\n\
+            ■ ■■■ ■       ■ ■■■ ■\n\
+            ■ ■■■ ■       ■ ■■■ ■\n\
+            ■     ■       ■     ■\n\
+            ■■■■■■■       ■■■■■■■\n\
+            ",
+            "                     \n".repeat(7).as_str(),
+            "\
+            ■■■■■■■              \n\
+            ■     ■              \n\
+            ■ ■■■ ■              \n\
+            ■ ■■■ ■              \n\
+            ■ ■■■ ■              \n\
+            ■     ■              \n\
+            ■■■■■■■              \
+            "
+        ].join("")
+            .to_string()
+    )
+}
+
+pub struct Matrix<'a> {
+    bits: &'a Vec<u8>,
     matrix: Vec<Vec<bool>>,
 }
 
-impl Matrix {
-    pub fn new(data: Vec<u8>, version: usize) -> Matrix {
-        Matrix {
+impl<'a> Matrix<'a> {
+    fn build_matrix(&mut self) {
+        add_finder_patterns(&mut self.matrix);
+    }
+
+    pub fn new(data: &Vec<u8>, version: usize) -> Matrix {
+        let mut matrix = Matrix {
             bits: data,
             matrix: {
                 let size = version * 4 + 17;
@@ -17,6 +97,10 @@ impl Matrix {
 
                 matrix
             },
-        }
+        };
+
+        matrix.build_matrix();
+
+        matrix
     }
 }
